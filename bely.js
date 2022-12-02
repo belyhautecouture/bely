9f1eeac4c8fbba706d10574597aac147ffa35353
belyhautecouture <118690233+belyhautecouture@users.noreply.github.com>
2022-12-03 00:34:14 +0700

    Update bely.js

--- bely.js
+++ bely.js
@@ -1,12 +1,12 @@
-window.addEventListener("load", () => {
-// console.log("window.onload")
-// document.querySelector('.facebook.link>img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/facebook.png')
-// document.querySelector('.zalo.link>img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/zalo.png');
-//  document.querySelector('.instgram.link>img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/instagram.png')
-//    document.querySelector('.youtube.link>img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/youtube.png')
-//    document.querySelector('.addThis_item--icon>.img-fluid')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/phone.png')
-//  document.querySelector('.addThis_listing.list-unstyled.d-none.d-sm-block>.addThis_item:nth-child(2)>a>img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/zalo.png')
-// //   document.querySelector('[alt="Phương thức thanh toán"]')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/payment.png')
-  document.querySelector('.product-promotion__heading.rounded-sm.d-inline-flex.align-items-center img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/gift.png')
-}
-)
+// window.addEventListener("load", () => {
+// // console.log("window.onload")
+// // document.querySelector('.facebook.link>img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/facebook.png')
+// // document.querySelector('.zalo.link>img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/zalo.png');
+// //  document.querySelector('.instgram.link>img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/instagram.png')
+// //    document.querySelector('.youtube.link>img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/youtube.png')
+// //    document.querySelector('.addThis_item--icon>.img-fluid')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/phone.png')
+// //  document.querySelector('.addThis_listing.list-unstyled.d-none.d-sm-block>.addThis_item:nth-child(2)>a>img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/zalo.png')
+// // //   document.querySelector('[alt="Phương thức thanh toán"]')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/payment.png')
+//   document.querySelector('.product-promotion__heading.rounded-sm.d-inline-flex.align-items-center img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/gift.png')
+// }
+// )
