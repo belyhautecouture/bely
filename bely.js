93e9d75bebc14be6669d53f6d3c747a73e753371
belyhautecouture <118690233+belyhautecouture@users.noreply.github.com>
2022-12-02 23:59:01 +0700

    Update bely.js

--- bely.js
+++ bely.js
@@ -6,7 +6,7 @@ document.querySelector('.zalo.link>img')?.setAttribute('src','https://belyhautec
    document.querySelector('.youtube.link>img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/youtube.png')
    document.querySelector('.addThis_item--icon>.img-fluid')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/phone.png')
  document.querySelector('.addThis_listing.list-unstyled.d-none.d-sm-block>.addThis_item:nth-child(2)>a>img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/zalo.png')
-   document.querySelector('[alt="Phương thức thanh toán"]')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/payment.png')
+//   document.querySelector('[alt="Phương thức thanh toán"]')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/payment.png')
   document.querySelector('.product-promotion__heading.rounded-sm.d-inline-flex.align-items-center img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/gift.png')
 }
 )
