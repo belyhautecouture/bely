b3547388626dd93bf7c80b3b7127e734796e4f47
belyhautecouture <118690233+belyhautecouture@users.noreply.github.com>
2022-12-02 23:03:08 +0700

    Update bely.js

--- bely.js
+++ bely.js
@@ -1,4 +1,5 @@
 window.onload = ()=>{
+console.log("window.onload")
 document.querySelector('.facebook.link>img').setAttribute('src','https://belyhautecouture.github.io/bely/img/facebook.png')
 
   
