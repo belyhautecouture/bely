c2e94227e9b0d94b26a52464d76d4dadf304a739
belyhautecouture <118690233+belyhautecouture@users.noreply.github.com>
2022-12-04 21:14:11 +0700

    Update en.js

--- en.js
+++ en.js
@@ -1,12 +1,13 @@
+// <script language="javascript" src="https://belyhautecouture.github.io/bely/en.js"></script>
 var btn = document.createElement("button");
 btn.innerText = "ENGLISH";
 var banner = document.querySelector('.banner');
 banner?.insertAdjacentElement('beforeBegin',btn);
 
-btn.setAttribute('onclick',"changeLanguage")
+btn.setAttribute('onclick',"changeLanguage()")
 btn.style.padding = '15px';
 
 var is_vietnam = document.querySelector('#is_vietnam')
 function changeLanguage(){
-if (is_vietnam.value==false){is_vietnam.value= true;console.log("TIẾNG VIỆT")} else {is_vietnam.value=false;btn.innerText="Tiếng Việt";console.log("TIẾNG ANH")}
+if (is_vietnam.value==false){is_vietnam.value= true;btn.innerText="ENGLISH";console.log("TIẾNG VIỆT")} else {is_vietnam.value=false;btn.innerText="Tiếng Việt";console.log("TIẾNG ANH")}
 }
