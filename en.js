39402d2093284db8d24ffd4dbd32c2baa590a9cd
belyhautecouture <118690233+belyhautecouture@users.noreply.github.com>
2022-12-04 10:13:10 +0700

    Update en.js

--- en.js
+++ en.js
@@ -1,3 +1,9 @@
 let btn = document.createElement("button");
 btn.innerText = "ENGLISH";
 document.body.appendChild(btn);
+
+btn.setAttribute('onclick',"changeLanguage")
+
+var is_vietnam = document.querySelector('#is_vietnam')
+function changeLanguage(){
+if (is_vietnam.value==false){is_vietnam.value= true}} else {is_vietnam.value=false;btn.innerText="Tiếng Việt"}
