63dc403a92cc734fddcce84ca6ed880291e0101a
belyhautecouture <118690233+belyhautecouture@users.noreply.github.com>
2022-12-07 14:43:58 +0700

    Update bely.js

--- bely.js
+++ bely.js
@@ -1,4 +1,4 @@
-// window.addEventListener("load", () => {
+window.addEventListener("load", () => {
 // // console.log("window.onload")
 // // document.querySelector('.facebook.link>img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/facebook.png')
 // // document.querySelector('.zalo.link>img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/zalo.png');
@@ -8,7 +8,8 @@
 // //  document.querySelector('.addThis_listing.list-unstyled.d-none.d-sm-block>.addThis_item:nth-child(2)>a>img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/zalo.png')
 // // //   document.querySelector('[alt="Phương thức thanh toán"]')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/payment.png')
 //   document.querySelector('.product-promotion__heading.rounded-sm.d-inline-flex.align-items-center img')?.setAttribute('src','https://belyhautecouture.github.io/bely/img/gift.png')
-// }
-// )
+
 if (window.innerWidth<=991&&window.location.href.indexOf('she-wearbely')>0){document.querySelector('.heading-bar').setAttribute('style','display:none')}
 if (window.innerWidth<=991&&window.location.href.indexOf('best-friends-forever')>0){document.querySelector('.heading-bar').setAttribute('style','display:none')}
+}
+)
